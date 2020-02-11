6fcd14a66bbbfe91273fe79c3b003b8af6d2d129
wullensm <root@sib.fr>
2020-02-11 16:17:17 +0100

    ajout des fichiers au niveau du repository intitule myrepo_git

---
+++ README.txt
@@ -0,0 +1,5 @@
+je participe au sujet de formation "gerez votre code avec git et github" propose par OpenClassRoom. 
+
+Ce fichier README.txt fait part de l activite de fin de partie 2. 
+
+Mickael Wullens
---
+++ chpwd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+######################################################################
+######  Modification du mot de passe administrateur ####
+#
+# Auteur : M.Wullens
+# Date : 20180918
+#
+user=$1
+
+# step 1 : generer un mdp aleatoire de 24 caracteres
